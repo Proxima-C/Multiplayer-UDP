550eca6f1c7f74286fc257d86eb329eed22fa96c
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-12-03 15:58:37 +0200

    Add Server class in GameServer project

---
+++ GameServer/GameServer/Server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    public class Server
+    {
+        public static int MaxPlayers { get; set; }
+        public static int Port { get; set; }
+
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+        private static TcpListener tcpListener;
+
+        public static void Start(int maxPlayers, int port)
+        {
+            MaxPlayers = maxPlayers;
+            Port = port;
+
+            Console.WriteLine("Starting server...");
+            InitializeServerData();
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(TCPConnectCallback, null);
+
+            Console.WriteLine($"Server started on port {Port}.");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(TCPConnectCallback, null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed to connect: Server full!");
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
+        }
+    }
+}
