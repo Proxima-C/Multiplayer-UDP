d0c4baeb103ac77f9c3315b3e7dddde8554c0d6b
VladyslavTretiak <54894036+Proxima-C@users.noreply.github.com>
2021-12-03 18:11:54 +0200

    Add UDP server-client communication in GameServer

--- GameServer/GameServer/ServerSend.cs
+++ GameServer/GameServer/ServerSend.cs
@@ -8,6 +8,12 @@
             Server.clients[_toClient].tcp.SendData(_packet);
         }
 
+        private static void SendUDPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Server.clients[_toClient].udp.SendData(_packet);
+        }
+
         private static void SendTCPDataToAll(Packet _packet)
         {
             _packet.WriteLength();
@@ -28,6 +34,27 @@
             }
         }
 
+        private static void SendUDPDataToAll(Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                Server.clients[i].udp.SendData(_packet);
+            }
+        }
+        private static void SendUDPDataToAll(int _exceptClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                if (i != _exceptClient)
+                {
+                    Server.clients[i].udp.SendData(_packet);
+                }
+            }
+        }
+
+        #region Packets
         public static void Welcome(int _toClient, string _msg)
         {
             using (Packet _packet = new Packet((int)ServerPackets.welcome))
@@ -38,5 +65,16 @@
                 SendTCPData(_toClient, _packet);
             }
         }
+
+        public static void UDPTest(int _toClient)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.udpTest))
+            {
+                _packet.Write("A test packet for UDP.");
+
+                SendUDPData(_toClient, _packet);
+            }
+        }
+        #endregion
     }
 }
